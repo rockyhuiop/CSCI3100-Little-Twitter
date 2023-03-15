ede3cab54ca6ea0b35f25432fb4491cd2c882a64
MarkCSC <cscmarkchan@gmail.com>
2023-03-16 00:04:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const https = require('https')
+const fs = require("fs");
+
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+const bodyParser = require('body-parser');
+
 
 mongoose.set('strictQuery', true)
 dotenv.config()
@@ -10,6 +17,7 @@ dotenv.config()
 const home_guest = require('./routes/homeGuest-route.js')
 const home_account = require('./routes/homeAccount-route.js')
 const login = require('./routes/login-route.js')
+const logout = require('./routes/logout-route.js')
 const registration = require('./routes/registration-route.js')
 const profile = require('./routes/profile-route.js')
 const user = require('./routes/user-route.js')
@@ -19,10 +27,28 @@ const dashboard = require('./routes/dashboard-route.js')
 app = express()
 router = express.Router()
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(sessions({
+    secret: process.env.SECRET,
+    saveUninitialized:true,
+    cookie: { maxAge: 86400000 },
+    resave: false 
+}));
+
 //pages
 app.use('/', home_guest)
-app.use('/home', home_account)
 app.use('/login', login)
+
+app.use((req, res, next)=>{
+    if(!req.session.userid){
+        console.log('not logged in, redirect')
+        return res.redirect(`https://${req.headers.host}/login`)
+    }
+    next()
+})
+
+app.use('/home', home_account)
+app.use('/logout', logout)
 app.use('/registration', registration)
 app.use('/profile', profile)
 app.use('/user', user)
@@ -40,5 +66,16 @@ mongoose
         useUnifiedTopology: true
     })
     .then(console.log(`Connected to mongoDB`))
-    .then(()=> {app.listen(process.env.PORT || 5000, ()=>console.log(`Server running on port: ${process.env.PORT}`))})
+    //.then(()=> {app.listen(process.env.PORT || 5000, ()=>console.log(`Server running on port: ${process.env.PORT}`))})
     .catch((err)=>{console.log(`Error: ${err}`)})
+
+https
+    .createServer(
+        {
+            key: fs.readFileSync("key.pem"),
+            cert: fs.readFileSync("cert.pem"),
+        },
+        app)
+    .listen(process.env.PORT || 5000, ()=>{
+      console.log(`server is runing at port ${process.env.PORT}`)
+    });
