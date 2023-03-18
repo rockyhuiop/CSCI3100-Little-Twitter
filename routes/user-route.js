bb8afe9ee742f48c4b7f9d808c2c657885c8833b
Chan Siu Chung <cscmarkchan@gmail.com>
2023-03-19 05:47:21 +0800

    Update user-route.js

--- routes/user-route.js
+++ routes/user-route.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 router = express.Router()
 
+router.get('/',(req, res)=>{
+    res.status(200).send(`<h1>user page</h1>`)
+})
+
 router.get('/:id',(req, res)=>{
     const { id } = req.params
     console.log(`${id}`)
