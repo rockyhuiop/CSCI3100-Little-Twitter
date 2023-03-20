2d6a8a4b939630d2516a76d3c5754e45101ada0b
AnsonCHOWcm <ccm19990901@gmail.com>
2023-03-21 03:41:35 +0800

    Created Tweet Schema

---
+++ model/Tweet.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const TweetSchema = new Schema({
+    tweetID:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    CreatorUserID:{
+        type: String,
+        required: true,
+    },
+    Content:{
+        type: String,
+        required: true,
+    },
+    LikeCount:{
+        type: Number,
+        required: true,
+    },
+    DisLikeCount:{
+        type: Number,
+        required: true,
+    },
+    Status:{
+        type: Boolean,
+        required: true,
+    },
+});
+
+module.exports = User = mongoose.model("Tweet", TweetSchema);
