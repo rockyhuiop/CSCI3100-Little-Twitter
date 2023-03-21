70f5062dd390ff3c8261afe84a0489d85181b849
Chan Siu Chung <cscmarkchan@gmail.com>
2023-03-22 02:57:07 +0800

    Update user-route.js

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,15 +1,31 @@
 const express = require('express')
+const User = require('../model/User.js')
 
 router = express.Router()
 
 router.get('/',(req, res)=>{
-    res.status(200).send(`<h1>user page</h1>`)
+    return res.status(200).send(`<h1>user page</h1>`)
 })
 
-router.get('/:id',(req, res)=>{
+router.get('/:id', async (req, res)=>{
     const { id } = req.params
-    console.log(`${id}`)
-    res.status(200).send(`<h1>${id}</h1>`)
+    try {
+        let user = await User.find({ tweetID: id}).select('-password');
+        if (user) {
+            return res.status(200).json({
+                data: user,
+                state: "Success"
+            })
+        } else {
+            return res.status(200).json({ error: "User doesn't exist" });
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: error });
+    }
+    
 })
+
+
 
 module.exports = router
