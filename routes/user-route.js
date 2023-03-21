16b1453f2171bdd1fab520e86b6bbad897953d8b
Chan Siu Chung <cscmarkchan@gmail.com>
2023-03-22 03:11:58 +0800

    Update user-route.js
    can now use id and name

--- routes/user-route.js
+++ routes/user-route.js
@@ -3,14 +3,28 @@ const User = require('../model/User.js')
 
 router = express.Router()
 
-router.get('/',(req, res)=>{
-    return res.status(200).send(`<h1>user page</h1>`)
+router.get('/', async (req, res)=>{
+    let name = req.query.name
+    try {
+        let user = await User.find({ name: name}, 'name tweetID');
+        if (user) {
+            return res.status(200).json({
+                data: user,
+                state: "Success"
+            })
+        } else {
+            return res.status(200).json({ error: "User doesn't exist" });
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: error });
+    }
 })
 
 router.get('/:id', async (req, res)=>{
     const { id } = req.params
     try {
-        let user = await User.find({ tweetID: id}).select('-password');
+        let user = await User.findOne({ tweetID: id}).select('-password');
         if (user) {
             return res.status(200).json({
                 data: user,
@@ -25,7 +39,5 @@ router.get('/:id', async (req, res)=>{
     }
     
 })
-
-
 
 module.exports = router
