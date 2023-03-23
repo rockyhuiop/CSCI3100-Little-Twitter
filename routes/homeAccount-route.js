ebb23df588c4f8dd822b374f9fe4764e5672d972
AnsonCHOWcm <ccm19990901@gmail.com>
2023-03-24 01:37:32 +0800

    create tweeet-route

--- routes/homeAccount-route.js
+++ routes/homeAccount-route.js
@@ -1,10 +1,6 @@
 const express = require('express')
-const Tweet = require("../model/Tweet")
-const {
-    nextTweetID,
-    AmendTweetLike,
-    AmendTweetDisLike,
-} = require('../controller/tweet')
+
+const tweet = require('./tweet-route')
 
 router = express.Router()
 
@@ -12,63 +8,6 @@ router.get('/', (req, res)=>{
     res.status(200).send(`<h1>logged in Home page</h1>`)
 })
 
-router.post('/', (req, res)=>{
-
-    ( async () => {
-        var tweetCount = await nextTweetID()
-        tweetCount = tweetCount +1
-        
-        const newTweet = new Tweet({
-            tweetID : tweetCount,
-            CreatorUserID : req.session.userid,
-            Content : req.body.Content,
-            LikeCount: 0,
-            DisLikeCount: 0,
-            SuspensionStatus: false
-        })
-        newTweet.save()
-    }
-    )()
-
-    res.status(200).json({message: "success, posted tweet"})
-})
-
-//Double Tick Like Tweet 
-router.patch('/like/:tweetID', (req, res)=>{
-
-    // counting the total number of tweet existing
-    ( async () => {
-
-        const {tweetID:tweetID} = req.params
-        const userID = req.session.userid
-
-        await AmendTweetLike(tweetID, userID)
-
-        res.status(200).json({
-            state: "Success"
-        })
-    }
-    )()
-
-})
-
-//Double Tick DisLike Tweet 
-router.patch('/dislike/:tweetID', (req, res)=>{
-
-    // counting the total number of tweet existing
-    ( async () => {
-
-        const {tweetID:tweetID} = req.params
-        const userID = req.session.userid
-
-        await AmendTweetDisLike(tweetID, userID)
-
-        res.status(200).json({
-            state: "Success"
-        })
-    }
-    )()
-
-})
+router.use('/', tweet)
 
 module.exports = router
---
+++ routes/tweet-route.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const Tweet = require("../model/Tweet")
+const {
+    nextTweetID,
+    AmendTweetLike,
+    AmendTweetDisLike,
+} = require('../controller/tweet')
+
+router = express.Router()
+
+//Posting Tweet
+router.post('/', (req, res)=>{
+
+    ( async () => {
+        var tweetCount = await nextTweetID()
+        tweetCount = tweetCount +1
+        
+        const newTweet = new Tweet({
+            tweetID : tweetCount,
+            CreatorUserID : req.session.userid,
+            Content : req.body.Content,
+            LikeCount: 0,
+            DisLikeCount: 0,
+            SuspensionStatus: false
+        })
+        newTweet.save()
+    }
+    )()
+
+    res.status(200).json({message: "success, posted tweet"})
+})
+
+//Double Tick Like Tweet 
+router.patch('/like/:tweetID', (req, res)=>{
+
+    // counting the total number of tweet existing
+    ( async () => {
+
+        const {tweetID:tweetID} = req.params
+        const userID = req.session.userid
+
+        await AmendTweetLike(tweetID, userID)
+
+        res.status(200).json({
+            state: "Success"
+        })
+    }
+    )()
+
+})
+
+//Double Tick DisLike Tweet 
+router.patch('/dislike/:tweetID', (req, res)=>{
+
+    // counting the total number of tweet existing
+    ( async () => {
+
+        const {tweetID:tweetID} = req.params
+        const userID = req.session.userid
+
+        await AmendTweetDisLike(tweetID, userID)
+
+        res.status(200).json({
+            state: "Success"
+        })
+    }
+    )()
+
+})
+
+module.exports = router
