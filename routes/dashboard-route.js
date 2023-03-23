776c92b2d97be0ffffbe99c1a845075447ab7c54
Chan Siu Chung <cscmarkchan@gmail.com>
2023-03-23 18:06:39 +0800

    Update dashboard-route.js
    delete user and show all user function done

--- routes/dashboard-route.js
+++ routes/dashboard-route.js
@@ -1,9 +1,40 @@
 const express = require('express')
+const User = require('../model/User.js')
 
 router = express.Router()
 
-router.get('/', (req, res)=>{
-    res.status(200).send(`<h1>Dashboard page</h1>`)
+//middleware checking if the user is admin
+router.use((req, res, next)=>{
+    if(req.session.userType !== "admin"){
+        return res.status(401).json({error: "No permission"})
+    }
+    next()
 })
+
+router.get('/', async (req, res)=>{
+
+    let user = await User.find({})
+
+    return res.status(200).json({
+        data: user,
+        state: "Success"
+    }) 
+})
+
+router.delete('/:tweetID', async (req, res)=>{
+    //check if user is admin
+    if(req.session.userType !== "admin"){
+        return res.status(401).json({error: "No permission"})
+    }
+
+    console.log(req.params.tweetID)
+    let result = await User.deleteOne({tweetID: req.params.tweetID})
+
+    return res.status(200).json({
+        data: result,
+        state: "Success"
+    }) 
+})
+
 
 module.exports = router
