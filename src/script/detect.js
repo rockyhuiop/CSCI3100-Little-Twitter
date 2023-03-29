421ccdb497277d7c1052935a47d5187fcc7458db
24asx4 <24asasasas@gmail.com>
2023-03-30 07:23:09 +0800

    Update detect.js
    make it able to be use for login and reg popup as well

--- src/script/detect.js
+++ src/script/detect.js
@@ -1,7 +1,13 @@
 export const detect = event =>{
-    var add_tw_click=document.getElementById("hp-addtw-click");
+    var popup_click=document.getElementsByClassName("hp-popup-click");
     var add_tw=document.getElementsByClassName("hp-addtw")[0];
-    if (event.target === add_tw_click) {
+    var log=document.getElementsByClassName("hp-log")[0];
+    var reg=document.getElementsByClassName("hp-reg")[0];
+    if (event.target === popup_click[0]) {
         add_tw.style.display="none";
+    } else if (event.target === popup_click[1]){
+        log.style.display="none";
+    } else if (event.target === popup_click[2]){
+        reg.style.display="none";
     }
 }
