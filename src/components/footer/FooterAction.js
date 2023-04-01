9304a15d5fff65efc8c727e2eef4b533d4b21eb4
24asx4 <24asasasas@gmail.com>
2023-04-01 14:29:48 +0800

    Update FooterAction.js
    fix the login and reg popup

--- src/components/footer/FooterAction.js
+++ src/components/footer/FooterAction.js
@@ -1,25 +1,31 @@
 import { toggle_div } from "../../script/toggle_div";
 import styles from "./FooterAction.module.css";
+import Login from "../login";
+import Reg from "../reg";
 
 const FooterAction = () => {
     return (
-        <div className={styles.footer}>
-            <p>Login to enjoy more function</p>
-            <div className={styles["button-group"]}>
-                <button
-                    id="hp-foot-reg"
-                    onClick={() => toggle_div(1, "hp-reg")}
-                >
-                    Register
-                </button>
-                <button
-                    id="hp-foot-log"
-                    onClick={() => toggle_div(1, "hp-log")}
-                >
-                    Login
-                </button>
+        <>
+            <div className={styles.footer}>
+                <p>Login to enjoy more function</p>
+                <div className={styles["button-group"]}>
+                    <button
+                        id="hp-foot-reg"
+                        onClick={() => toggle_div(1, "hp-reg")}
+                    >
+                        Register
+                    </button>
+                    <button
+                        id="hp-foot-log"
+                        onClick={() => toggle_div(1, "hp-log")}
+                    >
+                        Login
+                    </button>
+                </div>
             </div>
-        </div>
+            <Login />
+            <Reg />
+        </>
     );
 };
 export default FooterAction;
