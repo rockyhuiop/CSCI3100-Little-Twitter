2b9dba0c9522d1c8fd908b33026ee503df017ad0
24asx4 <24asasasas@gmail.com>
2023-04-03 16:13:27 +0800

    no longer useful

--- src/script/toggle_div.js
+++
@@ -1,8 +0,0 @@
-export const toggle_div = (status,cl) =>{
-    var div=document.getElementsByClassName(cl)[0];
-    if (status===0)
-        div.style.display="none";
-    else
-        div.style.display="block";
-        
-}
