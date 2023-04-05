5bf9293bc6d9b6835f39995038abce60fb39240d
rockyhuiop <rockyhuiop@gmail.com>
2023-04-05 19:05:07 +0800

    add tweet files

---
+++ src/components/tweet/TweetDetail.js
@@ -0,0 +1,71 @@
+import { faBookmark } from "@fortawesome/free-regular-svg-icons";
+import { faUserXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import IconMenu from "../reusable/IconMenu.js";
+import "./TweetDetail.css";
+import TweetActions from "./TweetActions.js";
+
+const TweetDetails = ({ tweet }) => {
+    const tweetStatistic = {
+        commentCount: tweet.commentCount,
+        retweetCount: tweet.retweetCount,
+        likeCount: tweet.likeCount,
+        viewCount: tweet.viewCount,
+    };
+
+    // const userUrl = "/" + tweet.user.userId;
+    const userUrl = "/profile";
+    // const tweetUrl = userUrl + "/" + tweet.tweetId;
+    const tweetUrl = userUrl + "/tweet";
+
+    return (
+        <div className="tweet__static">
+            <div className="tweet__header">
+                <div className="tweet__headercontainer">
+                    <a href={userUrl}>
+                        <img
+                            src={tweet.user.profile_image_url}
+                            alt="Avatar"
+                            className="tweet__avatar"
+                        />
+                    </a>
+                </div>
+                <div className="tweet__container">
+                    <div className="tweet__title">
+                        <div className="tweet__userinfo">
+                            <span className="tweet__username">
+                                <a href={userUrl}>{tweet.user.name}</a>{" "}
+                            </span>
+                            <span className="tweet__uid">
+                                @{tweet.user.userId}
+                            </span>
+                            <span className="tweet__timestamp">
+                                &nbsp;· 2h ago
+                            </span>
+                        </div>
+                        <div
+                            onClick={(e) => {
+                                e.preventDefault();
+                            }}
+                        >
+                            <IconMenu
+                                clickHandlers={[null, null]}
+                                icons={[
+                                    <FontAwesomeIcon icon={faUserXmark} />,
+                                    <FontAwesomeIcon icon={faBookmark} />,
+                                ]}
+                                names={["Unfollow", "Bookmark"]}
+                                keySuffix={tweet.tweetId}
+                            />
+                        </div>
+                    </div>
+                    <div className="tweet__content">{tweet.text}</div>
+                    <TweetActions tweetStatistic={tweetStatistic} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TweetDetails;
