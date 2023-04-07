575a4a73680b06d8d9d3798b2ef642a78c5f25ba
Chan Siu Chung <cscmarkchan@gmail.com>
2023-04-07 15:39:32 +0800

    Update profile-route.js

--- routes/profile-route.js
+++ routes/profile-route.js
@@ -1,9 +1,67 @@
 const express = require('express')
+const User = require('../model/User.js')
+//setting options for multer
+const multer = require('multer')
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router = express.Router()
 
-router.get('/', (req, res)=>{
-    res.status(200).json({state: "Sucess", data: {}})
+router.get('/', async (req, res)=>{
+    try {
+        let user = await User.findOne({ tweetID: req.session.userid}).select('-password');
+        if (user) {
+            return res.status(200).json({
+                data: user,
+                state: "Success"
+            })
+        } else {
+            return res.status(200).json({ error: "User doesn't exist" });
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: error });
+    }
+})
+
+router.post('/update', upload.fields([{ name: "avatar" }, { name: "banner" }]) ,async (req, res)=>{
+    // req.file can be used to access all file properties
+  try {
+    let user = await User.findOne({ tweetID: req.session.userid});
+
+    let {name, email, password} = req.body
+    //check if the request has an image or not
+    if (!req.files.avatar || !req.files.banner) {
+      return res.status(400).json({
+        error: "both file is needed"
+      });
+    } else {
+      user.avatar.data = req.files['avatar'][0].buffer
+      user.avatar.contentType = req.files['avatar'][0].buffer
+      // saving the object into the database
+      user.banner.data = req.files['banner'][0].buffer
+      user.banner.contentType = req.files['banner'][0].buffer
+      
+    }
+    if(name){
+        user.name = name
+    }
+    if(email){
+        user.email = email 
+    }
+    if(password){
+        user.password = password
+    }
+
+    await user.save()
+    return res.status(200).json({
+        state: "Success",
+        data: user
+    })
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
 })
 
 module.exports = router
