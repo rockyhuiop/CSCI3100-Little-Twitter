dc0af37c1fc01ede0abc6a84fe6a18d6c9f8edd4
Chan Siu Chung <cscmarkchan@gmail.com>
2023-04-07 21:12:57 +0800

    Update profile-route.js
    update the logic

--- routes/profile-route.js
+++ routes/profile-route.js
@@ -30,18 +30,14 @@ router.post('/update', upload.fields([{ name: "avatar" }, { name: "banner" }]) ,
     let user = await User.findOne({ tweetID: req.session.userid});
 
     let {name, email, password} = req.body
-    //check if the request has an image or not
-    if (!req.files.avatar || !req.files.banner) {
-      return res.status(400).json({
-        error: "both file is needed"
-      });
-    } else {
-      user.avatar.data = req.files['avatar'][0].buffer
-      user.avatar.contentType = req.files['avatar'][0].mimetype
-      // saving the object into the database
-      user.banner.data = req.files['banner'][0].buffer
-      user.banner.contentType = req.files['banner'][0].mimetype
-      
+
+    if(req.files.avatar){
+        user.avatar.data = req.files['avatar'][0].buffer
+        user.avatar.contentType = req.files['avatar'][0].mimetype
+    }
+    if(req.files.banner){
+        user.banner.data = req.files['banner'][0].buffer
+        user.banner.contentType = req.files['banner'][0].mimetype
     }
     if(name){
         user.name = name
