cbed918376ae67a7add28728762f0e38f0bf792d
Chan Siu Chung <cscmarkchan@gmail.com>
2023-04-09 07:30:52 +0800

    Update user-route.js

--- routes/user-route.js
+++ routes/user-route.js
@@ -42,72 +42,56 @@ router.get('/:id', async (req, res)=>{
 })
 
 //follow user
-router.post('/:id', async(req, res)=>{
-    const { id } = req.params
-    let user = User.findOne({tweetID: id})
+router.post('/:id', async (req, res) => {
+    const { id } = req.params;
+    let user = await User.findOne({ tweetID: id });
+  
+    try {
+      if (user.followers !== undefined && user.followers.includes(req.session.userid)) {
+        const result1 = await User.updateOne(
+          { "tweetID": id },
+          { $pull: { "followers": `${req.session.userid}` } }
+        );
+  
+        if (result1.nModified === 0) {
+          return res.status(400).json({ error: "no user found" });
+        }
+  
+        const result2 = await User.updateOne(
+          { "tweetID": req.session.userid },
+          { $pull: { "followings": `${id}` } }
+        );
+  
+        if (result2.nModified === 0) {
+          return res.status(400).json({ error: "no user found" });
+        }
+      } else {
+        const result3 = await User.updateOne(
+          { "tweetID": id },
+          { $push: { "followers": `${req.session.userid}` } }
+        );
+  
+        if (result3.nModified === 0) {
+          return res.status(400).json({ error: "no user found" });
+        }
+  
+        const result4 = await User.updateOne(
+          { "tweetID": req.session.userid },
+          { $push: { "followings": `${id}` } }
+        );
+  
+        if (result4.nModified === 0) {
+          return res.status(400).json({ error: "no user found" });
+        }
+      }
 
-    if(user.followers.includes(req.session.userid)){
-        await User.updateOne(
-            { tweetID: id },
-            { $pull: { followers : req.session.userid } },
-            function(err, result) {
-                if (err) {
-                console.log(err);
-                return res.status(500).json({ error: "Server error" })
-                } else if (result.nModified === 0) {
-                console.log('No documents were modified.');
-                return res.status(400).json({ error: "no user found" })
-                }
-            }
-        );
-    
-        await User.updateOne(
-            { tweetID: req.session.id },
-            { $pull: { followings : id } },
-            function(err, result) {
-                if (err) {
-                console.log(err);
-                return res.status(500).json({ error: "Server error" })
-                } else if (result.nModified === 0) {
-                console.log('No documents were modified.');
-                return res.status(400).json({ error: "no user found" })
-                }
-            }
-        );
-    }else{
-        await User.updateOne(
-            { tweetID: id },
-            { $push: { followers : req.session.userid } },
-            function(err, result) {
-                if (err) {
-                console.log(err);
-                return res.status(500).json({ error: "Server error" })
-                } else if (result.nModified === 0) {
-                console.log('No documents were modified.');
-                return res.status(400).json({ error: "no user found" })
-                }
-            }
-        );
-    
-        await User.updateOne(
-            { tweetID: req.session.id },
-            { $push: { followings : id } },
-            function(err, result) {
-                if (err) {
-                console.log(err);
-                return res.status(500).json({ error: "Server error" })
-                } else if (result.nModified === 0) {
-                console.log('No documents were modified.');
-                return res.status(400).json({ error: "no user found" })
-                }
-            }
-        );
+      return res.status(200).json({
+        state: "Success",
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Server error" });
     }
-    
-    return res.status(200).json({
-        state: "Success"
-    })
-
-})
+  });
 
 module.exports = router
