dd6ee6e54570c0759834951fbbba90f3b3c155e3
24asx4 <24asasasas@gmail.com>
2023-04-09 16:30:29 +0800

    [not yet finished] making the fetch of 2 tab

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Tabs from "../reusable/Tabs";
 import Tweet from "../tweet/Tweet";
 import styles from "./filter.module.css";
@@ -24,14 +24,57 @@ const Filter = () => {
             },
         },
     ]);
+    const tw_pop=[]
+    const tw_rec=[]
+    useEffect(() => {
+        const fetchall = async () => {
+            const not_login = await fetch("/home/FetchAllTweet", {
+                method: "GET",
+                headers: {
+                    "Content-Type":
+                        "application/x-www-form-urlencoded;charset=UTF-8",
+                },
+            });
+            const not_log_json = await not_login.json();
+            
+            const new_tw = [];
+            for (var i = 0; i < not_log_json.message.length; i++) {
+                new_tw.push({
+                    tweetId: i,
+                    text: not_log_json.message[i].Content,
+                    user: {
+                        userId: not_log_json.message[i].CreatorUserID,
+                        name: not_log_json.message[i].CreatorUserName,
+                        profile_image_url:
+                            "https://pbs.twimg.com/profile_images/1632814091319508994/cwm-3OQE_400x400.png",
+                    },
+                    media: "",
+                    date: "",
+                    likeCount: not_log_json.message[i].LikeCount,
+                    commentCount: not_log_json.message[i].CommentCount,
+                    retweetCount: not_log_json.message[i].ReTweetCount,
+                    viewCount: 1000,
+                });
+            }
+            console.log(not_log_json.message)
+            setTweets(new_tw);
+            tw_pop=tweets.sort((a,b)=>b.viewCount-a.viewCount);
+            tw_rec=tweets.sort((a,b)=>b.date-a.date);
+        }
+        fetchall();
+    }, []);
     return (
         <Tabs tabNames={["Popular", "Recent"]}>
             <div className={styles.con}>
-                {tweets.map((tweet) => (
+                {tw_pop.map((tweet) => (
                     <Tweet key={tweet.tweetId} tweet={tweet} />
                 ))}
             </div>
-            <div className={styles.con}>second tab test</div>
+            <div className={styles.con}>
+                {tw_rec.map((tweet) => (
+                    <Tweet key={tweet.tweetId} tweet={tweet} />
+                ))}
+            </div>
         </Tabs>
     );
 };
