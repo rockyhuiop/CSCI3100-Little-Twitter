20fe5fc60cf69803bbaf0b1534d4f4ac284f12bb
Chan Siu Chung <cscmarkchan@gmail.com>
2023-04-11 05:18:20 +0800

    Update logout-route.js

--- routes/logout-route.js
+++ routes/logout-route.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const User = require('../model/User.js')
+const Follow = require('../model/Follow.js')
 
 router = express.Router()
 
-router.get('/', (req, res)=>{
+router.get('/', async (req, res)=>{
     console.log('user logout')
+    let user_id = req.session.userid
     req.session.destroy()
-    res.status(200).json({
+    
+    let user = await User.findOne({tweetID: user_id})
+
+    if(user){
+        try {
+            if(user.followers){
+                await Follow.updateOne({tweetID: user_id},{tweetID: user.tweetID, followers: user.followers },{upsert: true})
+            }
+        } catch (error){
+            return res.status(500).json({error: "failed to save user status"})
+        }
+
+    }else{
+        return res.status(200).json({error: "cannot save user info"})
+    }
+    return res.status(200).json({
         state: "Success",
     })
 })
