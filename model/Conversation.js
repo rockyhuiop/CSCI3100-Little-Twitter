d07454b94c0dbb5a9fc9a6df8dea53c22d941887
AnsonCHOWcm <ccm19990901@gmail.com>
2023-04-11 10:56:10 +0800

    typo

--- model/Conversation.js
+++ model/Conversation.js
@@ -1,4 +1,4 @@
-const mongoose = requrie("mongoose")
+const mongoose = require("mongoose")
 
 const ConservationSchema = new mongoose.Schema(
     {
--- model/Message.js
+++ model/Message.js
@@ -1,4 +1,4 @@
-const mongoose = requrie("mongoose")
+const mongoose = require("mongoose")
 
 const MessageSchema = new mongoose.Schema(
     {
