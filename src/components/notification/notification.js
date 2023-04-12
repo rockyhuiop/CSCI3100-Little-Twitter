8e2ffd10ab79ce8995c5bd8d1434cd27789f83a2
24asx4 <24asasasas@gmail.com>
2023-04-12 11:26:02 +0800

    Add notifications function

--- src/components/notification/notification.js
+++ src/components/notification/notification.js
@@ -1,29 +1,60 @@
-import { useEffect} from "react";
+import { useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../../utils/UserContext";
 import styles from "./notification.module.css"
+import Message from "./message"
 
 
 const Notification = () => {
     const { isLoggedIn } = useUser();
     const nav = useNavigate();
+    const [fol,setFol] = useState([
+
+    ]);
     useEffect(() => {
         if (!isLoggedIn) {
             nav("/", { replace: true });
+        } else{
+            const fetchNotif = async() =>{
+                const new_fol=[];
+                const notification = await fetch("/user/notification/",
+                    {
+                        method: "GET",
+                        headers: {
+                            "Content-Type":
+                                "application/x-www-form-urlencoded;charset=UTF-8",
+                        },
+                    }
+                );
+                const notification_json = await notification.json();
+                for (var i=0;i<notification_json.data.length;i++){
+                    new_fol.push(
+                        {
+                        tweetID: notification_json.data[i].tweetID,
+                        name: notification_json.data[i].name,
+                        id: notification_json.data[i]._id,
+                        },
+                    );
+                }
+                setFol(new_fol);
+            }
+
+            fetchNotif();
+            
         }
-    });
+    },[]);
     return (
         <>
             <div className={styles.title}>Notification</div>
+            {fol.length==0 ? 
             <div className={styles.message}>
-                Testing message 1
+                There is no new followers.
             </div>
-            <div className={styles.message}>
-                Testing message 2
-            </div>
-            <div className={styles.message}>
-                Testing message 3
-            </div>
+            :
+            fol.map((fols) => (
+                <Message key={fols.id} fols={fols} />
+            ))
+            }
         </>
     );
 };
