bdad56f787a29498bd21872ccd292056123d093f
AnsonCHOWcm <ccm19990901@gmail.com>
2023-04-28 21:32:46 +0800

    finished Chatbox

--- routes/conservations-route.js
+++ routes/conservations-route.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Conversation = require("../model/Conversation")
+const User = require('../model/User.js')
 
 router = express.Router()
 
@@ -58,6 +59,39 @@ router.get("/search/:firstUserID/:secondUserID", (req, res) => {
     })()
 
 })
+
+router.get("/search/:firstUserID", (req, res) => {
+
+    (async() => {
+        const {firstUserID:firstUserID} = req.params
+        try{
+            const conversation = await Conversation.find({
+                members : {$in: [firstUserID]},
+            })
+            console.log(conversation)
+            console.log(conversation.length)
+            var conservatedUserID = []
+            var conservatedmember = []
+            for (let i = 0, len = conversation.length; i<len; i++){
+                conservatedmember = conversation[i].members
+                
+                if (conservatedmember[0] == firstUserID){
+                    conservatedUserID.push(conservatedmember[1])
+                }else{
+                    conservatedUserID.push(conservatedmember[0])
+                }
+            }
+            const user = await User.find({tweetID: {$in: conservatedUserID}})
+            res.status(200).json({data: user})
+        }catch(err){
+            console.log(err)
+            res.status(500).json(err)
+        }
+    })()
+
+})
+
+//creating the conversation for inputed IDs
 
 router.post("/create/:firstUserID/:secondUserID", (req, res) => {
     (async() => {
