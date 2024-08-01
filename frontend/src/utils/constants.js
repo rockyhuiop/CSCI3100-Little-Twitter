0de6ea422aec874a0678f2e62a791d5935cb2cd9
24asx4 <24asasasas@gmail.com>
2024-08-01 20:44:53 +0800

    fix image link

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -4,4 +4,4 @@
 
 // address of the API server. Normally it should be stored
 // as an environment variable but who cares?
-export const SERVER_ADDRESS = "https://localhost:8000/";
+export const SERVER_ADDRESS = "/";
